4999e6db363b3f0d99e087b649a061f39659f78d
Jamilo-school <119764861+Jamilo-school@users.noreply.github.com>
2023-11-06 04:19:16 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,128 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyefqfqskFC2lfy8cm-TCqAYf1BHoeb91nyuGKqn29B5YzhGmZbsN78W4qqTG3UgnFv9A/exec';
+const form = document.forms['submit-to-google-sheet'];
+const userNameDisplay = document.getElementById('userNameDisplay'); // Get the element for displaying the user's name
+let submittedData = JSON.parse(localStorage.getItem('submittedData')) || []; // Retrieve data from local storage
+
+// Function to update local storage with submitted data
+function updateLocalStorage() {
+  localStorage.setItem('submittedData', JSON.stringify(submittedData));
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const nameInput = form.querySelector('input[name="Official name"]').value;
+  const gradeInput = form.querySelector('select[name="Grade"]').value;
+  const idNumberInput = form.querySelector('input[name="idNumber"]').value; // Get the ID number input value
+
+  // Check if the same name, class, and ID number combination already exists
+  const exists = submittedData.some(entry => entry.name === nameInput && entry.grade === gradeInput && entry.idNumber === idNumberInput);
+
+  if (exists) {
+    Swal.fire({
+      title: 'Duplicate Entry',
+      text: 'A duplicate entry for the same name, class, and ID number has been detected. Do you want to proceed?',
+      icon: 'error',
+      showCancelButton: true,
+      confirmButtonText: 'Accept',
+      cancelButtonText: 'Cancel',
+      footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // User confirmed, proceed with the submission
+        Swal.fire({
+          title: '🧑‍⚕️Submitting...',
+          text: 'Please wait',
+          icon: 'info',
+          showConfirmButton: false,
+          allowOutsideClick: false,
+        });
+
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+          .then(response => {
+            Swal.close();
+            if (response.status === 200) {
+              // Display the user's name, ID number on a new line, and in uppercase in the success popup
+              Swal.fire({
+                title: 'Success!',
+                html: `Submission Successful!<br><span style="text-transform: uppercase;">Name: ${nameInput}</span><br><span style="text-transform: uppercase;">ID Number: ${idNumberInput}</span>`,
+                icon: 'success',
+                footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+              });
+              form.reset();
+              // Add the submitted data to the array
+              submittedData.push({ name: nameInput, grade: gradeInput, idNumber: idNumberInput });
+              updateLocalStorage(); // Update local storage
+            } else {
+              Swal.fire({
+                title: 'Error!',
+                text: 'Submission Failed',
+                icon: 'error',
+                footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+              });
+            }
+          })
+          .catch(error => {
+            console.error('Error!', error.message);
+            Swal.close();
+            Swal.fire({
+              title: 'Error!',
+              text: 'Submission Failed',
+              icon: 'error',
+              footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+            });
+          });
+      }
+    });
+  } else {
+    Swal.fire({
+      title: '🧑‍⚕️Submitting...',
+      text: 'Please wait',
+      icon: 'info',
+      showConfirmButton: false,
+      allowOutsideClick: false,
+    });
+
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => {
+        Swal.close();
+        if (response.status === 200) {
+          // Display the user's name, ID number on a new line, and in uppercase in the success popup
+          Swal.fire({
+            title: 'Success!',
+            html: `Submission Successful!<br><span style="text-transform: uppercase;">Name: ${nameInput}</span><br><span style="text-transform: uppercase;">ID Number: ${idNumberInput}</span>`,
+            icon: 'success',
+            footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+          });
+          form.reset();
+          // Add the submitted data to the array
+          submittedData.push({ name: nameInput, grade: gradeInput, idNumber: idNumberInput });
+          updateLocalStorage(); // Update local storage
+        } else {
+          Swal.fire({
+            title: 'Error!',
+            text: 'Submission Failed',
+            icon: 'error',
+            footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+          });
+        }
+      })
+      .catch(error => {
+        console.error('Error!', error.message);
+        Swal.close();
+        Swal.fire({
+          title: 'Error!',
+          text: 'Submission Failed. Please check your internet Connection',
+          icon: 'error',
+          footer: 'Powered by Mr. Oduor Macro' // Add the footer here
+        });
+      });
+  }
+});
+
+
+
+
+
+
+
