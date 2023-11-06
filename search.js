f5e4171d98b013e4676f3c8add443d181471ce0d
Jamilo-school <geofreyonyango167@gmail.com>
2023-11-06 04:35:42 +0300

    oo

--- search.js
+++ search.js
@@ -16,6 +16,7 @@ function searchAndDisplay() {
     var textNodes = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
     var idCounter = 1;
     var foundMatchingText = false; // Initialize a flag for found text
+    var matchingTextList = []; // Create a list to store matching text
 
     while (textNodes.nextNode()) {
         var textNode = textNodes.currentNode;
@@ -29,6 +30,7 @@ function searchAndDisplay() {
             resultElement.id = resultId;
             idCounter++;
             foundMatchingText = true; // Set the flag to true if matching text is found
+            matchingTextList.push(textContent); // Add matching text to the list
         }
     }
 
@@ -40,10 +42,11 @@ function searchAndDisplay() {
             text: 'Text Not Found',
         });
     } else {
-        // If matching text was found, show a success SweetAlert
+        // If matching text was found, show a success SweetAlert with the list of matching text
         Swal.fire({
             icon: 'success',
             title: 'Found Text Matching',
+            html: matchingTextList.join('<br>') // Display the matching text in the pop-up
         });
     }
 }
